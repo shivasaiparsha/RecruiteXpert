72d930bfa568a74aaa09be3f8d075a455807409c
Shantanu pawar <shantanupawar292@gmail.com>
2024-02-03 20:48:40 +0530

    structure

---
+++ src/main/java/com/tool/RecruitXpert/DTO/file.java
@@ -0,0 +1,4 @@
+package com.tool.RecruitXpert.DTO;
+
+public class file {
+}
---
+++ src/test/java/com/tool/RecruitXpert/RecruitXpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tool.RecruitXpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitXpertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
